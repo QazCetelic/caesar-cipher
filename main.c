e5b7ee7c23cb1b51c14e3eb6e3e0908bfc47d932
QazCetelic <qaz.cetelic@protonmail.com>
2023-06-05 17:08:41 +0200

    Only change letters and use pointer arithmetic instead of int to count loop

--- main.c
+++ main.c
@@ -10,18 +10,19 @@ int main(int argc, char *argv[]) {
     char *path = argv[1];
     long offset = strtol(argv[2], NULL, 10);
 
-    if (offset == 0) {
+    const long MAX_OFFSET = 1000;
+    if (offset == 0 || offset > MAX_OFFSET || offset < -MAX_OFFSET) {
         printf("Invalid offset '%s'\n", argv[2]);
         return 1;
     }
-
-    printf("Applied an offset of %ld to the file %s\n", offset, path);
 
     FILE *file_input = fopen(path, "r");
     if (file_input == NULL) {
         puts("File not found");
         return 1;
     }
+
+    printf("Applied an offset of %ld to the file %s\n", offset, path);
 
     fseek(file_input, 0, SEEK_END);
     long size = ftell(file_input);
@@ -31,16 +32,13 @@ int main(int argc, char *argv[]) {
     fread(buff, size, 1, file_input);
     fclose(file_input);
 
-    for (long i = size - 1; i > 0; i--) {
-        const int MIN = 33;
-        const int MAX = 126;
-        if (buff[i] >= MIN && buff[i] <= MAX) {
-            long val = (long) buff[i];
-            val = (val - MIN + offset) % (MAX - MIN + 1) + MIN;
-            buff[i] = (char) val;
+    char *str = buff;
+    for (; *str != '\0'; ++str) {
+        if (*str >= 'a' && *str <= 'z') {
+            *str = (*str - 'a' + offset) % 26 + 'a';
         }
-        else {
-            buff[i] = buff[i];
+        else if (*str >= 'A' && *str <= 'Z') {
+            *str = (*str - 'A' + offset) % 26 + 'A';
         }
     }
 
